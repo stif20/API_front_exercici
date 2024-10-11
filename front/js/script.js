c509de878261deef02ce1ec8706d21ef79b5b1d9
A.T. <atalens1@xtec.cat>
2024-10-11 13:10:50 +0200

    exercici

---
+++ front/js/script.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Cridem a l'endpoint de l'API fent un fetch
+    ______________________________________________
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Error a la resposta del servidor");
+            }
+            return response.json();
+        })
+        .then(data => {
+            const alumnesTableBody = document.querySelector("#tablaAlumne tbody");
+            alumnesTableBody.innerHTML = ""; // Netejar la taula abans d'afegir res
+            
+            // Iterar sobre los alumnos y agregarlos al DOM
+            data.forEach(alumne => {
+                const row = document.createElement("tr");
+
+                const idCell = document.createElement("td");
+                idCell.textContent = alumne.IdAlumne;
+                row.appendChild(idCell);
+
+                // Repetir per tots els altres camps restants que retorna l'endpoint
+                _____________________________________________
+
+                
+
+                alumnesTableBody.appendChild(row);
+            });
+        })
+        .catch(error => {
+            console.error("Error capturat:", error);
+            alert("Error al carregar la llista d'alumnes");
+        });
+});
